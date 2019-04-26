3cd243ca92e49f23e344c53e9336dc60006bbc35
Timothy Shores <timothymshores@gmail.com>
2019-04-26 10:56:25 -0500

    Create GET route handler to /api/actions/:id

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -13,4 +13,18 @@ actionsRouter.get('/', (req, res) => {
         })
 });
 
+actionsRouter.get('/:id', (req, res) => {
+    const message404 = { error: "The action with the specified ID does not exist." }
+    const message500 = { error: "The action information could not be retrieved." }
+
+    Actions
+        .get(req.params.id)
+        .then(project => {
+            project
+                ? res.status(200).json(project)
+                : res.status(404).json(message404);
+        })
+        .catch(err => { res.status(500).json(message500) })
+});
+
 module.exports = actionsRouter;
