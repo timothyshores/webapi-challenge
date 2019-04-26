71c5fa2b110fc1a59795eab7f99961a2a2dfcbbe
Timothy Shores <timothymshores@gmail.com>
2019-04-26 10:35:26 -0500

    Installed express via yarn

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -2,4 +2,15 @@ const express = require('express');
 const Projects = require('./projectModel');
 const projectsRouter = express.Router();
 
+projectsRouter.get('/', (req, res) => {
+    const message500 = { error: "The projects information could not be retrieved." };
+
+    Projects
+        .get()
+        .then(projects => { res.status(200).json(projects) })
+        .catch(err => {
+            res.status(500).json(message500);
+        })
+});
+
 module.exports = projectsRouter;
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 
-const postsRouter = require('./actions/actionsRouter');
-const usersRouter = require('./projects/projectsRouter');
+const actionsRouter = require('./actions/actionsRouter');
+const projectsRouter = require('./projects/projectsRouter');
 
 const server = express();
-
-// Configure global middleware
 server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send(`
     <h2>Web 18 - Web API Sprint Challenge </h2>
+    <p>Actions Endpoints</p>
+    <ul>
+        <li>GET /api/actions </li>
+        <li>GET /api/actions/:id </li>
+        <li>POST /api/actions </li>
+        <li>DELETE /api/actions/:id</li>
+        <li>PUT /api/actions/:id</li>
+    </ul>
+    <p>Projects Endpoints</p>
+    <ul>
+        <li>GET /api/projects </li>
+        <li>GET /api/projects/:id </li>
+        <li>POST /api/projects </li>
+        <li>DELETE /api/projects/:id</li>
+        <li>PUT /api/projects/:id</li>
+    </ul>
     `);
 });
 
