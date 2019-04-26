57aeb6bad73bf732cd5b44aac7fe1d61d81c635d
Timothy Shores <timothymshores@gmail.com>
2019-04-26 10:41:03 -0500

    Create GET route handler to /api/actions

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -2,4 +2,15 @@ const express = require('express');
 const Actions = require('./actionModel');
 const actionsRouter = express.Router();
 
+actionsRouter.get('/', (req, res) => {
+    const message500 = { error: "The actions information could not be retrieved." };
+
+    Actions
+        .get()
+        .then(actions => { res.status(200).json(actions) })
+        .catch(err => {
+            res.status(500).json(message500);
+        })
+});
+
 module.exports = actionsRouter;
