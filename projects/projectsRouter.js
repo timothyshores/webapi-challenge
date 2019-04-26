fd931d0743d3dcacbf6a173156cb35ef0ef1c4ff
Timothy Shores <timothymshores@gmail.com>
2019-04-26 10:44:23 -0500

    Create GET route handler to /api/projects/:id

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -13,4 +13,18 @@ projectsRouter.get('/', (req, res) => {
         })
 });
 
+projectsRouter.get('/:id', (req, res) => {
+    const message404 = { error: "The project with the specified ID does not exist." }
+    const message500 = { error: "The project information could not be retrieved." }
+
+    Projects
+        .get(req.params.id)
+        .then(project => {
+            project
+                ? res.status(200).json(project)
+                : res.status(404).json(message404);
+        })
+        .catch(err => { res.status(500).json(message500) })
+});
+
 module.exports = projectsRouter;
